f090fae8bfc54a7f819506b5ca67f2a52764ed0c
ChanYu Chen <cychen214@gmail.com>
2018-12-13 10:53:57 +0800

    added hunter power test page

---
+++ hunter-power-test/precache-manifest.ae18c7d47242fd1173423940aec824cf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ebf3771ddb88187f036a",
+    "url": "/hunter-power-test/static/css/main.e19f3d7f.chunk.css"
+  },
+  {
+    "revision": "ebf3771ddb88187f036a",
+    "url": "/hunter-power-test/static/js/main.ebf3771d.chunk.js"
+  },
+  {
+    "revision": "1eba6647638554cb24ec",
+    "url": "/hunter-power-test/static/js/1.1eba6647.chunk.js"
+  },
+  {
+    "revision": "b93db3a314cc08760068",
+    "url": "/hunter-power-test/static/js/runtime~main.b93db3a3.js"
+  },
+  {
+    "revision": "817e80f0a67794ed07c5cd87ce2cec50",
+    "url": "/hunter-power-test/index.html"
+  }
+];
